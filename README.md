eed6c9c1e8efb124e138ff6914b29bb8824d3b24
Judith <judith@earthpulse.ai>
2024-04-23 12:34:53 +0200

    docker and minikube working again

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-# ml-inference
-ML Inference Processor
+# ML Inference Processor
 
 A docker image has been created and runned using the Dockerfile:
 
@@ -8,7 +7,7 @@ A docker image has been created and runned using the Dockerfile:
 ```
 
 ```bash
- docker run --name ml-inference -p 8000:8000 ml-inference
+ docker run --name ml-inference -p 8000:8000 --name ml-inf -d ml-inference
 ```
 
 Or altrenatively, using the docker compose configuration file:
@@ -20,9 +19,28 @@ docker-compose up
 The image has been pushed to dockerhub with this commands:
 
 ```bash
-docker tag ml-inference judithep/ml-inference:v1.0
+docker tag ml-inference judithep/ml-inference:v0
 ```
 
 ```bash
-docker push judithep/ml-inference:v1.0
+docker push judithep/ml-inference:v0
+```
+
+Once published in DockerHub, it has been deployed in a local kubernettes cluster using `Minikube`and `kubectl`:
+
+1. Install minikube and kubectl
+2. Using a docker image of minikube, start the cluster in local:
+
+```bash
+minikube start
+```
+3. Deploy to the cluster the image published in DockerHub:
+
+```bash
+kubectl create deployment ml-inference --image=judithrp/ml-inference:v0
+```
+4. Run a proxy to access the app:
+
+```bash
+kubectl proxy
 ```
--- api/main.py
+++ api/main.py
@@ -10,6 +10,7 @@ app = FastAPI()
 
 @app.post("/{model}")
 async def inference(model: str, img_file: UploadFile = File(...)):
+    
     # Read the data from the file in memory
     contents = await img_file.read()
 
@@ -18,15 +19,21 @@ async def inference(model: str, img_file: UploadFile = File(...)):
         with memfile.open() as dataset:
             # Convert the file content to a numpy array
             img_array = dataset.read()
-
+    
     try:
-        download_path = download_model(model, force=True)
+        download_path = download_model(model, force=True) # it should point to STAC of the model
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=str(e))
+    
+    # TODO: Check the specifications of the array 
+    # TODO: Check the STAC of the model to check the `input` requirements
+    # TODO: Compare the array with the `input` requirements        
+    # TODO: Do the preprocessing of the image if necessary
 
     # TODO: It seems that the model was not exported correctly to accept a random batch size (it only works with a batch size of 25)
     batch = np.array(25*[img_array.astype(np.float32)])
 
+    # TODO: Check the STAC of the model to check the `runtime` requirements and apply
     file_path = os.path.join(download_path, 'model.onnx')
     
     # Execute model
@@ -34,5 +41,10 @@ async def inference(model: str, img_file: UploadFile = File(...)):
     input_name = ort_session.get_inputs()[0].name
     ort_inputs = {input_name: batch}
     ort_outs = ort_session.run(None, ort_inputs)
+
+    # TODO: Check the STAC of the model to check the `output` requirements
+    # TODO: Check the specifications of the outputs
+    # TODO: Compare the outputs with the `output` requirements  
+    # TODO: Do the postprocessing of the outputs if necessary
 
     return {"message": f"{model} model executed", "outputs": f"{ort_outs[0][0]}"}
