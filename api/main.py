23b4f1b166e3d85b06462740171df667387b8ca4
juansensio <sensio.juan@gmail.com>
2024-08-19 13:18:27 +0200

    add support for Road Segmentation model

--- api/main.py
+++ api/main.py
@@ -11,7 +11,7 @@ from PIL import Image
 from src.utils import download_model, format_outputs, sigmoid
 from src.onnx import get_onnx_session
 
-__version__ = "2024.06.18"
+__version__ = "2024.08.19"
 
 app = FastAPI(
     title="ml-inference",
