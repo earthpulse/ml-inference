8470497614b1bd78cd1c3935c30c3035658dbd15
juansensio <sensio.juan@gmail.com>
2024-10-07 09:23:39 +0200

    update

--- api/main.py
+++ api/main.py
@@ -11,7 +11,7 @@ from PIL import Image
 from src.utils import download_model, format_outputs, sigmoid
 from src.onnx import get_onnx_session
 
-__version__ = "2024.09.27"
+__version__ = "2024.10.07"
 
 app = FastAPI(
     title="ml-inference",
