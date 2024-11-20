78f376c0ae983c9c04ab9fe72225bcff252c6b3c
juansensio <sensio.juan@gmail.com>
2024-11-20 14:10:02 +0100

    add eotdl model wrapper

--- api/main.py
+++ api/main.py
@@ -5,11 +5,10 @@ from starlette.responses import StreamingResponse
 import rasterio as rio
 import io
 import os
-import numpy as np
 from PIL import Image
 
-from src.utils import download_model, format_outputs, sigmoid
-from src.onnx import get_onnx_session
+from src.utils import sigmoid
+from src.eotdl_wrapper import ModelWrapper
 
 __version__ = "2024.10.07"
 
@@ -45,25 +44,8 @@ async def inference(
 ):
     try:
         # download model from ETODL
-        download_path, stac_df = download_model(model, DOWNLOAD_PATH, version)
-
-        # validate model metadata
-        items = stac_df[stac_df["type"] == "Feature"]
-        assert (
-            len(items) == 1
-        ), "Only one model is expected (multiple items found in STAC metadata)"
-        model_props = items.properties.values[0]
-        # print(model_props)
-        assert (
-            model_props["mlm:framework"] == "ONNX"
-        ), "Only ONNX models are supported (mlm:framework)"
-        model_name = model_props["mlm:name"]
-        model_path = f"{download_path}/assets/{model_name}"
-        # print(model_path)
-
-        # load model
-        ort_session = get_onnx_session(model_path)
-        # print(ort_session.get_provider_options())
+        # download_path, stac_df = download_model(model, DOWNLOAD_PATH, version)
+        model = ModelWrapper(model, path=DOWNLOAD_PATH, version=version)
 
         # load image in memory as numpy array
         with rio.open(io.BytesIO(image.file.read())) as src:
@@ -71,23 +53,7 @@ async def inference(
         assert image.ndim == 3, "Image must have 3 dimensions (bands, height, width)"
 
         # pre-process and validate input
-        input = model_props["mlm:input"]
-
-        # input data type
-        dtype = input["input"]["data_type"]
-        image = image.astype(dtype)
-
-        # input shape
-        input_shape = input["input"]["shape"]
-        ndims = len(input_shape)
-        if ndims != image.ndim:
-            if ndims == 4:
-                image = np.expand_dims(image, axis=0).astype(np.float32)
-            else:
-                raise Exception("Input shape not valid", input_shape, image.ndim)
-        for i, dim in enumerate(input_shape):
-            if dim != -1:
-                assert dim == image.shape[i], f"Input dimension not valid: The model expects {input_shape} but the image has {image.shape} (-1 means any dimension)."
+        image = model.process_inputs(image)
 
         # normalization
         image = (
@@ -97,28 +63,14 @@ async def inference(
         # TODO: resize if necessary
 
         # execute model
-        input_name = ort_session.get_inputs()[0].name
-        ort_inputs = {input_name: image}
-        ort_outs = ort_session.run(None, ort_inputs)
-
-        output_nodes = ort_session.get_outputs()
-        output_names = [node.name for node in output_nodes]
-
-        outputs = format_outputs(ort_outs, output_names, image, model_props)
+        outputs = model.predict(image)
 
         # return outputs
-        if model_props["mlm:output"]["tasks"] == ["classification"]:
-            return {
-                "model": model,
-                **outputs,
-            }
-        elif model_props["mlm:output"]["tasks"] == [
-            "segmentation"
-        ]:  # only returns first output as image
+        if model.props["mlm:output"]["tasks"] == ["classification"]:
+            return outputs
+        elif model.props["mlm:output"]["tasks"] == ["segmentation"]:  # only returns first output as image
             # return mask
-            batch = outputs[output_names[0]]
-            image = batch[0]
-            image = sigmoid(image) > 0.5  # this should be defined in the model metadata
+            image = sigmoid(outputs) > 0.5  # this should be defined in the model metadata
             if image.ndim == 3:  # get first band
                 image = image[0]
             img = Image.fromarray(image, mode="L")  # only returns binary mask
@@ -128,7 +80,7 @@ async def inference(
             return StreamingResponse(buf, media_type="image/tiff")
         else:
             raise Exception(
-                "Output task not supported", model_props["mlm:output"]["tasks"]
+                "Output task not supported", model.props["mlm:output"]["tasks"]
             )
 
     except Exception as e:
@@ -140,8 +92,8 @@ async def inference(
 @app.get("/{model}")
 def retrieve_model_metadata(model: str, version: int = None):
     try:
-        _, stac_df = download_model(model, DOWNLOAD_PATH, version, download=False)
-        return stac_df.to_json()
+        model = ModelWrapper(model, path=DOWNLOAD_PATH, version=version)
+        return model.gdf.to_json()
     except Exception as e:
         print("ERROR", "retrieve_model_metadata")
         print(e)
--- api/src/utils.py
+++ api/src/utils.py
@@ -3,45 +3,9 @@ import requests
 import geopandas as gpd
 import numpy as np
 
-from .dataframe import STACDataFrame
 
 EOTDL_API_URL = "https://api.eotdl.com/"
 EOTDL_API_KEY = os.getenv("EOTDL_API_KEY")
-
-
-def download_model(model_name, dst_path, version, force=False, download=True):
-    # check if model already downloaded
-    version = 1 if version is None else version
-    download_path = dst_path + "/" + model_name + "/v" + str(version)
-    if os.path.exists(download_path) and not force:
-        df = STACDataFrame.from_stac_file(download_path + f"/{model_name}/catalog.json")
-        return download_path, df
-    # check model exists
-    model, error = retrieve_model(model_name)
-    if error:
-        raise Exception(error)
-    if model["quality"] < 2:
-        raise Exception("Only Q2+ models are supported")
-    # check version exist
-    assert version in [
-        v["version_id"] for v in model["versions"]
-    ], f"Version {version} not found"
-    # download model files
-    gdf, error = retrieve_model_stac(model["id"], version)
-    if error:
-        raise Exception(error)
-    df = STACDataFrame(gdf)
-    if not download:
-        return download_path, df
-    os.makedirs(download_path, exist_ok=True)
-    df.to_stac(download_path)
-    df = df.dropna(subset=["assets"])
-    for row in df.iterrows():
-        for k, v in row[1]["assets"].items():
-            href = v["href"]
-            _, filename = href.split("/download/")
-            download_file_url(href, filename, f"{download_path}/assets")
-    return download_path, df
 
 
 def format_response(response):
@@ -79,26 +43,6 @@ def download_file_url(url, filename, path):
                 if chunk:
                     f.write(chunk)
         return path
-
-
-def format_outputs(ort_outputs, output_names, input, model_props):
-    if model_props["mlm:output"]["tasks"] == ["classification"]:
-        return {
-            output: ort_outputs[i].tolist() for i, output in enumerate(output_names)
-        }
-    elif model_props["mlm:output"]["tasks"] == ["segmentation"]:
-        return {output: ort_outputs[i] for i, output in enumerate(output_names)}
-        # return {
-        #     output: reshape(
-        #         ort_outputs[i],
-        #         input,
-        #         model_props["mlm:output"]["result"]["shape"],
-        #         model_props["mlm:input"]["input"]["shape"],
-        #     )
-        #     for i, output in enumerate(output_names)
-        # }
-    else:
-        raise Exception("Output task not supported", model_props["mlm:output"]["tasks"])
 
 
 def reshape(x, input, output_shape, input_shape):
